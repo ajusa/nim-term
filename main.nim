1f3444d2951afaf10c6def8cc6d4918d415194bd
Arham Jain <arhamjain@gmail.com>
2019-04-04 20:51:06 -0400

    hey I can wrap lines this took two hours

--- main.nim
+++ main.nim
@@ -1,19 +1,23 @@
-import os, strformat, system, strutils
+import os, strformat, system, strutils, unicode
 import illwill
 
 proc exitProc() {.noconv.} =
   illwillDeinit()
   quit(0)
 
-var str = ""
-
-
+var str = getEnv("DYNALIST_KEY")
 illwillInit(fullscreen=true)
 setControlCHook(exitProc)
 
+proc writeWrap(tb: var TerminalBuffer, x, y: Natural, s: string): Natural =
+  var lines = wordWrap(s, tb.width - x).splitLines()
+  for i, str in lines: tb.write(x, y + i, str)
+  return y + lines.len #last line we write to
+
 while true:
   var tb = newTerminalBuffer(terminalWidth(), terminalHeight())
-  tb.write(0,0, str)
+  discard tb.writeWrap(10,0, str)
+  #tb.write(0,1, $tb[0, 0].ch)
   var key = getKey()
   case key
   of Key.Backspace: 
